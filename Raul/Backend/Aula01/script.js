fc48488594956d0ac39d26f74be7bb56a242971b
Raulqo <166512978+Raulqo@users.noreply.github.com>
2024-07-01 11:48:33 -0300

    Add files via upload

---
+++ Raul/Backend/Aula01/script.js
@@ -0,0 +1,42 @@
+/*const nome = "Raul"
+let sobrenome = "Quaresma de Oliveira"
+var idade = "15"
+
+//template string
+console.log(`meu nome é ${nome} ${sobrenome}, tenho ${idade} anos de idade.`)
+
+//Numbers
+let mes = 7
+const temperatura = -2
+
+let num1 = 22
+let num2 = 8
+
+console.log(num1 - num2)
+
+//booleans 
+let SouEstudante = true
+let SouProfessor = false*/
+
+
+//Crie uma variavel com o nome, a idade e uma variavel que diga se você é estudante ou não. imprima a frase no console.
+/*let nome = "Raul Quaresma de Oliveira"
+let idade = 15
+let estudante = true
+
+console.log(`Meu nome é ${nome} tenho ${idade} anos de idade. Sou estudante? ${estudante} `)
+console.log(typeof idade, typeof estudante)*/
+
+//passando strings para numbers
+const num1 = '50'
+const num2 = '70'
+const newNum1 = Number(num1)
+const newNum2 = Number(num2)
+const soma = newNum1 + newNum2
+console.log(soma)
+
+//Passando Numbers para strings
+const num3 = 5 
+ console.log(typeof num3)//Number
+ const num3ToString = num3.toString()
+ console.log(typeof num3ToString)//String
